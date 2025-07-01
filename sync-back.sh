bb5d671b446e3baf77e42160678c6aa80578d3f8
Stephen Goncalves <stiefo.o@gmail.com>
2025-07-01 21:08:12 +0100

    initial comit

---
+++ sync-back.sh
@@ -0,0 +1,3 @@
+rsync -av $DEV/myconfigs/i3 $HOME/.config
+rsync -av $DEV/myconfigs/spaceship.zsh $HOME/.config
+rsync -av $DEV/myconfigs/ghostty $HOME/.config
---
+++ sync.sh
@@ -0,0 +1,3 @@
+rsync -av $HOME/.config/i3 $DEV/myconfigs
+rsync -av $HOME/.config/spaceship.zsh $DEV/myconfigs
+rsync -av $HOME/.config/ghostty $DEV/myconfigs
