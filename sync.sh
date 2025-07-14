017e68e4ca2c0575c61d954930ae7ce5be31c55f
Stephen Goncalves <stiefo.o@gmail.com>
2025-07-14 18:51:57 +0100

    adds zshrc

--- sync.sh
+++ sync.sh
@@ -5,3 +5,6 @@ rsync -a $SOURCE/i3 $DESTINATION
 rsync -a $SOURCE/spaceship.zsh $DESTINATION
 rsync -a $SOURCE/ghostty $DESTINATION
 rsync -a $SOURCE/polybar $DESTINATION
+
+# .zshrc
+rsync -a $HOME/.zshrc $DESTINATION
