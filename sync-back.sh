1852dcd74b49beb77d2a7b0db03a50afa77bd78a
Stephen Goncalves <stiefo.o@gmail.com>
2025-07-16 15:26:48 +0100

    fix sync back paths

--- sync-back.sh
+++ sync-back.sh
@@ -1,7 +1,7 @@
-SOURCE=$DEV/myconfigs
 DESTINATION=$HOME/.config
 
-rsync -a $SOURCE/i3 $DESTINATION
-rsync -a $SOURCE/spaceship.zsh $DESTINATION
-rsync -a $SOURCE/ghostty $DESTINATION
-rsync -a $SOURCE/polybar $DESTINATION
+rsync -a i3 $DESTINATION
+rsync -a spaceship.zsh $DESTINATION
+rsync -a ghostty $DESTINATION
+rsync -a polybar $DESTINATION
+rsync -a .zshrc $HOME
